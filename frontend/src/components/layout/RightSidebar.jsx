c9999df30e28328d13c98041fd0a3d2a34df7f27
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-05 11:53:28 +0545

    RightSIdebar

---
+++ frontend/src/components/layout/RightSidebar.jsx
@@ -0,0 +1,28 @@
+function RightSidebar(){
+
+return(
+
+<aside className="
+hidden
+xl:block
+w-72
+bg-white
+min-h-screen
+p-5
+border-l
+">
+
+<h2 className="
+font-bold
+">
+Suggestions
+</h2>
+
+</aside>
+
+)
+
+}
+
+
+export default RightSidebar;

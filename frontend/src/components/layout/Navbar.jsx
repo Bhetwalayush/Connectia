0d2900c2bd600518189bb4b283c1376d16cc4e3c
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-05 11:53:15 +0545

    Navbar

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,28 @@
+function Navbar() {
+
+  return (
+
+    <nav className="
+      h-16
+      bg-white
+      border-b
+      flex
+      items-center
+      px-6
+    ">
+
+      <h1 className="
+        text-2xl
+        font-bold
+        text-blue-600
+      ">
+        Connectia
+      </h1>
+
+    </nav>
+
+  );
+}
+
+
+export default Navbar;

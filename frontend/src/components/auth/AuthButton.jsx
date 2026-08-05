cd308205bf06bc1082a9708fe0713db4ef3f0a97
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-05 11:48:51 +0545

    Auth_Button

---
+++ frontend/src/components/auth/AuthButton.jsx
@@ -0,0 +1,30 @@
+function AuthButton({
+  children
+}) {
+
+return (
+
+<button
+
+className="
+w-full
+bg-blue-600
+text-white
+rounded-lg
+p-3
+font-semibold
+hover:bg-blue-700
+"
+
+>
+
+{children}
+
+</button>
+
+)
+
+}
+
+
+export default AuthButton;

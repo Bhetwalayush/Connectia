85916d27c21495d3e3233376372596d56f8cee10
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-05 11:56:36 +0545

    Validation

---
+++ frontend/src/utils/validators.js
@@ -0,0 +1,10 @@
+export function validateEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+
+export function validatePassword(password) {
+
+  return password.length >= 8;
+
+}

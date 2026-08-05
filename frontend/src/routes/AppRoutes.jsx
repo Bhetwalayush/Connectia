d274fc27ec6c7f92e331e938a778c0c198b4c544
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-05 11:56:27 +0545

    Routes

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import NotFound from "../pages/NotFound/NotFound";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+
+    <Route path="/" element={<Home />} />
+
+    </Route>
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;

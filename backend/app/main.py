a4aef5e796a94a553af52b941012b080c19b9b1d
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-10 08:12:44 +0545

    app_main

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+from app.graphql.schema import schema
+from app.graphql.context import get_context
+
+app = FastAPI()
+
+graphql_app = GraphQLRouter(
+    schema,
+    context_getter=get_context
+)
+
+app.include_router(
+    graphql_app,
+    prefix="/graphql"
+)

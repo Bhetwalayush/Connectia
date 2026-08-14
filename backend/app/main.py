186ab09e73e80fb91ff090c4d8546b48bbbe2c44
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-08-14 07:04:23 +0545

    Dependencies

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,39 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
+from strawberry.subscriptions import (
+    GRAPHQL_TRANSPORT_WS_PROTOCOL,
+    GRAPHQL_WS_PROTOCOL,
+)
 
 from app.graphql.schema import schema
 from app.graphql.context import get_context
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 graphql_app = GraphQLRouter(
     schema,
-    context_getter=get_context
+    context_getter=get_context,
+    subscription_protocols=[
+        GRAPHQL_TRANSPORT_WS_PROTOCOL,
+        GRAPHQL_WS_PROTOCOL,
+    ],
 )
 
 app.include_router(
     graphql_app,
-    prefix="/graphql"
+    prefix="/graphql",
 )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client/react";
 
+import { apolloClient } from "./graphql/client";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );

9696de1a5dff6ac26d407c37046d5d4a5c419882
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-09-15 13:50:19 +0545

    Global chat ui

--- frontend/src/pages/Explore/Explore.jsx
+++ frontend/src/pages/Explore/Explore.jsx
@@ -1,6 +1,11 @@
-﻿function Explore() {
-  return <h1>Explore Page</h1>;
+﻿import GlobalChatRoom from "../../components/explore/GlobalChatRoom";
+
+function Explore() {
+  return (
+    <div className="p-4">
+      <GlobalChatRoom />
+    </div>
+  );
 }
 
 export default Explore;
-

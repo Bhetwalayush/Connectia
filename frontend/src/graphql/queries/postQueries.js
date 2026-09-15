7d341f82467b08e5586a6e0177b9d454d019f573
Ayush Bhetwal <rujanbhetwal@gmail.com>
2026-09-15 13:49:54 +0545

    Getting post in scrollable manner

--- frontend/src/graphql/queries/postQueries.js
+++ frontend/src/graphql/queries/postQueries.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const GET_POSTS = gql`
-  query GetPosts {
-    posts {
+  query GetPosts($limit: Int, $offset: Int) {
+    posts(limit: $limit, offset: $offset) {
       id
       content
       imageUrl
